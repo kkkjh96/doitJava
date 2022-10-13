11a78e96d7197d31d4db175ae022b39de466b8a1
kkkjh96 <101761211+kkkjh96@users.noreply.github.com>
2022-10-13 16:38:57 +0900

    Add files via upload

---
+++ java/Chapter2/src/chapter2/DoubleEx.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class DoubleEx {
+	
+	public static void main(String[] args) {
+		double dNum = 3.14;
+		float fNum = 3.14F;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
+		var num = 10;
+		System.out.println(num);
+	}
+
+}
---
+++ java/Chapter3/src/operator/OperationEx5.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class OperationEx5 {
+
+	public static void main(String[] args) {
+
+		int num1 = 5;
+		int num2 = 11;
+		
+		int result = num1 | num2;
+		System.out.println(result);
+		
+		int num3 = 5; // 00000101;
+		System.out.println(num3 << 1);
+		System.out.println(num3 << 2);
+		System.out.println(num3 << 3);
+		
+		System.out.println(num3 >> 1); // 00000010;
+		System.out.println(num3 >> 2);
+		System.out.println(num3 >> 3);
+		
+		System.out.println(num3);
+	}
+
+}
