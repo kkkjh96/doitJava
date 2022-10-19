dbb140f9eda33f4f6787b868180d185f50047db2
kkkjh96 <101761211+kkkjh96@users.noreply.github.com>
2022-10-19 12:03:12 +0900

    Add files via upload

---
+++ java/Chapter5/src/reference/Subject.java
@@ -0,0 +1,29 @@
+package reference;
+
+public class Subject {
+
+	String subjectName;
+	int score;
+	
+	public Subject(String name) {
+		subjectName = name;
+	}
+	
+	public void seteSubjectName(String name) {
+		subjectName = name;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	
+}
